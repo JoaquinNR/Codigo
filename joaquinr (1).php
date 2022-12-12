bf22b521e47af8208fe887806442d57bf8779527
JoaquinNR <120412625+JoaquinNR@users.noreply.github.com>
2022-12-12 16:02:17 -0300

    Add files via upload

---
+++ joaquinr (1).php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+     <title>!Tarea Administracion Web¡ </title>
+
+</head>
+<body
+     bgcolor="orange"
+     text="black">
+<center>
+
+<?php
+class MyClass {
+      public static $str = "<h3>Mi nombre es Joaquín Rainao";
+      public static $JR  = "Asignatura: Administración de servicios Web <br>";
+
+
+}
+echo Myclass::$str."<br>";
+echo Myclass::$JR; "<br>";
+echo "La fecha de hoy es " .date("d/m/Y") . "<br>";
+date_default_timezone_set("America/Santiago");
+echo "La hora es: " .date("G:i:s");
+?>
+
+</center>
+</body>
+</html>
+   
